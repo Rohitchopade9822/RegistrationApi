2f5c9dc85788982bf66814e8fc9a55c3e5839611
Rohitchopade9822 <rohitprofessional99@gmail.com>
2024-07-17 15:25:19 +0530

    Add Repository

---
+++ RegistrationApi/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using RegistrationApi.DBModel;
+
+namespace RegistrationApi.Repository
+{
+    public interface IUserRepository
+    {
+        Userinfo GetUserByUsernameAndPassword(string username, string password);
+        void AddUser(Userinfo userinfo);
+        int GetusermaxId();
+        void SaveChanges();
+    }
+}
---
+++ RegistrationApi/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using RegistrationApi.DBModel;
+
+namespace RegistrationApi.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MyAppDbContext _context;
+
+        public UserRepository(MyAppDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public void AddUser(Userinfo userinfo)
+        {
+          return  
+        }
+
+        public Userinfo GetUserByUsernameAndPassword(string username, string password)
+        {
+          return   
+        }
+
+        public int GetusermaxId()
+        {
+          return   
+        }
+
+        public void SaveChanges()
+        {
+          return  
+        }
+    }
+}
